9e2d5f93c2067056db7a1323680512db7dd2f3b6
tri-0412 <157047428+tri-0412@users.noreply.github.com>
2024-10-07 16:59:10 +0700

    VideoComponents.js

--- src/layouts/components/Content/VideoComponents/VideoComponents.js
+++ src/layouts/components/Content/VideoComponents/VideoComponents.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import classNames from 'classnames/bind';
 import Styles from './VideoComponents.module.scss';
 import ReactPlayer from 'react-player';
